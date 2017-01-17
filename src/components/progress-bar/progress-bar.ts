a9dcb1504d42b636748b18516527606e881ccafc
Francisco Garcia <franciscogarcia@iMac-de-Francisco.local>
2017-01-17 19:26:37 +0100

    .

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input,Output,EventEmitter } from '@angular/core';
 
 /*
   Generated class for the ProgressBar component.
@@ -10,13 +10,26 @@ import { Component } from '@angular/core';
   selector: 'progress-bar',
   templateUrl: 'progress-bar.html'
 })
-export class ProgressBarComponent {
-
+export class ProgressBar {
+  @Input('total') totalSeconds:number = 100;
+  @Input('interval') intervalSeconds:number = 1;
+  @Output() refreshEvent = new EventEmitter();
+  
   text: string;
-
+  current: number;
+  step: number;
   constructor() {
     console.log('Hello ProgressBar Component');
     this.text = 'Hello World';
+    this.step = this.totalSeconds / this.intervalSeconds;
+    this.current = this.totalSeconds / this.intervalSeconds;
+  }
+
+  eachSecond(){
+    let i = setInterval(()=>{
+      this.refreshEvent.emit(this.current);
+      this.current = this.current + this.step;
+    },this.intervalSeconds*1000);
   }
 
 }
